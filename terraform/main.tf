8a3385c52966c6c9f0a78501cd1bf2842ce5f25c
quirkyQuack6 <vahrutdinov@gmail.com>
2026-07-28 01:58:28 +0500

    fix(terraform): correct backend region

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ terraform {
     bucket = "terraform-state"
     key    = "homelab/terraform.tfstate"
 
-    region = "us-east-1"
+    region = "eu-central-1"
 
     skip_credentials_validation = true
     skip_metadata_api_check     = true
