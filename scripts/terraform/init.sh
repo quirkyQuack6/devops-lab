ab144bd2c80d7d7b5fa9fae6cc9291a10effdbf9
quirkyQuack6 <vahrutdinov@gmail.com>
2026-07-28 00:54:42 +0500

    fix: migrate remote state to minio

--- scripts/terraform/init.sh
+++ scripts/terraform/init.sh
@@ -7,8 +7,6 @@ REPO_ROOT="$(cd "$SCRIPT_DIR/../.." && pwd)"
 cd "$REPO_ROOT"
 
 terraform -chdir=terraform init \
-		-reconfigure \
-		-migrate-state \
 		-input=false \
 		-backend-config="access_key=${AWS_ACCESS_KEY_ID}" \
 		-backend-config="secret_key=${AWS_SECRET_ACCESS_KEY}"
--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
   }
 
   backend "s3" {
-    endpoint = "http://192.168.2.21:9000"
+    endpoints.s3 = "http://192.168.2.21:9000"
 
     bucket = "terraform-state"
     key    = "homelab/terraform.tfstate"
