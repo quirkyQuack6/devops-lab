8f9f58b0f6412d4fceefcf33dc546f9361bdd4b7
quirkyQuack6 <vahrutdinov@gmail.com>
2026-08-03 09:37:36 +0500

    refactor: correct datetime to scan skipped images

--- scripts/test/run-trivy-images.sh
+++ scripts/test/run-trivy-images.sh
@@ -16,50 +16,48 @@ WORKSPACE="${HOST_WORKSPACE:-$PWD}"
 echo "Workspace for Docker: $WORKSPACE"
 
 docker run --rm \
-		-v trivy-cache:/root/.cache/trivy \
-		aquasec/trivy:0.72.0 \
-		image --download-db-only
+    -v trivy-cache:/root/.cache/trivy \
+    aquasec/trivy:0.72.0 \
+    image --download-db-only
 
 while read -r image; do
-		echo "Scanning $image..."
+    echo "Scanning $image..."
 
-		REPORT_FILE="$(echo "$image" | tr '/:' '__').json"
+    REPORT_FILE="$(echo "$image" | tr '/:' '__').json"
     success=false
-		LOG_FILE="$REPORT_DIR/$(echo "$image" | tr '/:' '__').error.log"
+    LOG_FILE="$REPORT_DIR/$(echo "$image" | tr '/:' '__').error.log"
 
-		for attempt in 1 2 3; do
-		    if timeout 5m docker run --rm \
-				     -v /var/run/docker.sock:/var/run/docker.sock \
-				     -v trivy-cache:/root/.cache/trivy \
-				     -v "$WORKSPACE/$REPORT_DIR":/reports \
-				     aquasec/trivy:0.72.0 \
-				     image \
-				     --scanners vuln \
-				     --no-progress \
-				     --skip-db-update \
-				     --format json \
-				     --output "/reports/$REPORT_FILE" \
-				     "$image" \
-						 >"$LOG_FILE" 2>&1;
+    for attempt in 1 2 3; do
+        if timeout 5m docker run --rm \
+            -v /var/run/docker.sock:/var/run/docker.sock \
+            -v trivy-cache:/root/.cache/trivy \
+            -v "$WORKSPACE/$REPORT_DIR":/reports \
+            aquasec/trivy:0.72.0 \
+            image \
+            --scanners vuln \
+            --no-progress \
+            --skip-db-update \
+            --format json \
+            --output "/reports/$REPORT_FILE" \
+            "$image" \
+            >"$LOG_FILE" 2>&1;
 		    then
-				    echo "✓ Scan completed"
-						rm -f "$LOG_FILE"
-						success=true
-				    break
-				fi
-				echo "Attempt $attempt failed"
-				if (( attempt != 3 )); then
-						echo "Retrying..."
-				fi
-		done
+            echo "✓ Scan completed"
+            rm -f "$LOG_FILE"
+            success=true
+            break
+        fi
+        echo "Attempt $attempt failed"
+        if (( attempt != 3 )); then
+            echo "Retrying..."
+        fi
+    done
 		
-		if [[ "$success" != true ]]; then
-				echo "Skipping $image"
-				printf "[%s] %s - failed after 3 attempts\n" \
-               "$(date '+%F %T')" \
-               "$image" \ 
-			         >> "$REPORT_DIR/skipped-images.txt"
-		fi
+    if [[ "$success" != true ]]; then
+        echo "Skipping $image"
+        printf "[%s] %s - failed after 3 attempts\n" \
+               "$(date '+%F %T')" "$image" >> "$REPORT_DIR/skipped-images.txt"
+    fi
 
 done < <(./scripts/test/get-docker-images.sh)
 
