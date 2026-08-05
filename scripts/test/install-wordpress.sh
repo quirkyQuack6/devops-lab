d6819188389577a59b917604ae264ffc1c08ddfe
quirkyQuack6 <vahrutdinov@gmail.com>
2026-08-05 18:31:36 +0500

    refactor: fixed the wpscan installation output

--- scripts/test/install-wordpress.sh
+++ scripts/test/install-wordpress.sh
@@ -7,17 +7,22 @@ cd "$REPO_ROOT"
 
 if docker compose -f test/docker-compose.test.yml run --rm \
 		wordpress-cli wp --url=http://wordpress core is-installed; then
-		echo "Alresdy installed"
-else
-		if docker compose -f test/docker-compose.test.yml run --rm\
-			   wordpress-cli wp core install \
-			   --url=http://wordpress \
-			   --title='testlab' \
-			   --admin_user=${VAULT_WP_ADMIN} \
-			   --admin_password=${VAULT_WP_ADMIN_PASS} \
-			   --admin_email=${VAULT_WP_EMAIL};
-    then
-       echo "Success: WordPress installed successfully.
+		echo "Already installed"
+    exit 0
+fi
+
+if docker compose -f test/docker-compose.test.yml run --rm \
+     wordpress-cli wp core install \
+     --url=http://wordpress \
+     --title='testlab' \
+			   --admin_user="${VAULT_WP_ADMIN}" \
+			   --admin_password="${VAULT_WP_ADMIN_PASS}" \
+			   --admin_email="${VAULT_WP_EMAIL}";
+then
+   echo "Success: WordPress installed successfully.
 			 
 			 Test environment is ready."
+else
+   echo "Error: WordPress installation failed."
+   exit 1
 fi
